5c10114eb6e98d7df9cf23114936d5b9e7cdea4a
lyfnsmile <li.zhixiang@live.cn>
2016-07-01 18:27:22 +0800

    123

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # fileUpload
+
+- 适用于文件和图片上传
+- 示例放在public/views目录下
+- 文件上传临时目录 temp
+- uploads保存最终文件目录
--- app.js
+++ app.js
@@ -1,22 +1,27 @@
 var express = require('express');
-var path=require('path');
+var path = require('path');
 var bodyParser = require('body-parser');
-var multer  = require('multer');
-var upload = multer({dest:'./uploads/'});
-
+var multer = require('multer');
+var upload = multer({ dest: 'temp/' });
+var fs = require('fs');
 
 var app = express();
 
 app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-app.use(express.static(path.join(__dirname, 'views')));
+app.use(bodyParser.urlencoded({ extended: false })); // for parsing application/x-www-form-urlencoded
 
-app.post('/avatar',upload.single('avatar'),function(req, res) {
-    console.log(upload.single('avatar'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.post('/avatar', upload.single('file'), function(req, res) {
     console.log(req.file);
-    res.status(200).json({ file: req.file});
+    var tmp_path = req.file.path;
+    var target_path = 'uploads/' + req.file.originalname;
+    var src = fs.createReadStream(tmp_path);
+    var dest = fs.createWriteStream(target_path);
+    src.pipe(dest);
+
+    res.status(200).json({ file: req.file });
 });
 
-app.listen(3000,function(){
-	console.log('服务已启动...')
+app.listen(3000, function() {
+    console.log('服务已启动...')
 })
