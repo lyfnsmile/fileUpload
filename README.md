c03a5fbfad9feb09677fdd0dab1b250d0ddb4da8
lyfnsmile <li.zhixiang@live.cn>
2016-07-29 15:52:38 +0800

    123

--- README.md
+++ README.md
@@ -4,3 +4,10 @@
 - 示例放在public/views目录下
 - 文件上传临时目录 temp
 - uploads保存最终文件目录
+
+### usage
+- npm install
+- npm start
+
+### reference
+- [http://stackoverflow.com/questions/31530200/node-multer-unexpected-field](http://stackoverflow.com/questions/31530200/node-multer-unexpected-field)
--- app.js
+++ app.js
@@ -7,21 +7,58 @@ var fs = require('fs');
 
 var app = express();
 
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: false })); // for parsing application/x-www-form-urlencoded
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.post('/avatar', upload.single('file'), function(req, res) {
-    console.log(req.file);
-    var tmp_path = req.file.path;
-    var target_path = 'uploads/' + req.file.originalname;
-    var src = fs.createReadStream(tmp_path);
-    var dest = fs.createWriteStream(target_path);
-    src.pipe(dest);
+// app.post('/avatar', upload.single('file'), function(req, res) {
+//     console.log(req.file);
+//     var tmp_path = req.file.path;
+//     var target_path = 'uploads/' + req.file.originalname;
+//     var src = fs.createReadStream(tmp_path);
+//     var dest = fs.createWriteStream(target_path);
+//     src.pipe(dest);
 
-    res.status(200).json({ file: req.file });
-});
+//     // function dataToImage(dataUrl) {
+//     //     var base64Data = dataUrl.split(",")[1];
+//     //     var dataBuffer = new Buffer(base64Data, 'base64');
 
-app.listen(3000, function() {
+//     //     fs.writeFile('out.jpg', dataBuffer, function(err) {
+//     //         if (err) {
+//     //             console.log(err);
+//     //         } else {
+//     //             console.log('Success...');
+//     //         }
+//     //     });
+//     // }
+
+
+//     res.status(200).json({ file: req.file });
+// });
+
+
+
+
+app.post('/avatar',function(req,res){
+    console.log(req.body)
+    dataToImage(req.body.result)
+    res.json(req.body)
+})
+
+app.listen(4000, function() {
     console.log('服务已启动...')
 })
+
+
+function dataToImage(dataUrl){
+    var base64Data = dataUrl.replace(/^data:image\/\w+;base64,/,'');
+    var dataBuffer = new Buffer(base64Data,'base64');
+
+    fs.writeFile('out.jpg',dataBuffer,function(err){
+        if(err){
+            console.log(err);
+        }else{
+            console.log('Success...');
+        }
+    });
+}
